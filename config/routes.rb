01913d144ec329106cd9f5c7fbb8e69351e9bdc9
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-22 15:27:16 +0000

    implement RESTFUL Api for Kittens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'kittens/index'
+  get 'kittens/create'
+  get 'kittens/new'
+  get 'kittens/update'
+  get 'kittens/destroy'
+  get 'kittens/show'
+  get 'kittens/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
